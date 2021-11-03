944118d9d425544045d77544fd146b1786c771f7
Matthew Berginski <matthew.berginski@gmail.com>
2021-11-03 16:06:15 -0400

    updates after talking with Matt

--- app.R
+++ app.R
@@ -7,38 +7,75 @@ library(dqshiny)
 library(readxl)
 library(ggpubr)
 library(png)
+library(patchwork)
 
-location_lookup = read_rds(here('data/location_lookup.rds'))
+location_lookup = read_rds(here('data/location_lookup.rds')) %>%
+    mutate(type = fct_relevel(type, c("Inflamed","Control")))
 
-exp_data = read_excel(here('data/All Data CTA.xlsx'), sheet = 5) %>%
-    select(-ScanLabel,-SegmentLabel) %>%
-    add_row(ROILabel = 099) %>%
-    add_row(ROILabel = 098) %>%
-    pivot_longer(-ROILabel,names_to = "gene", values_to = "exp") %>%
-    mutate(location = sprintf("pos_%03d",ROILabel)) %>%
+exp_data = read_csv(here('data/vogt_deseq2_norm_counts.csv')) %>%
+    mutate(gs099 = NA, gs098 = NA) %>% 
+    rename(gene = ...1) %>%
+    pivot_longer(-gene,names_to = "temp",values_to = "exp") %>%
+    separate(temp, into=c(NA,"number"),sep = 2) %>%
+    mutate(location = paste0("pos_",number)) %>%
+    select(-number) %>%
     left_join(location_lookup)
 
 img <- readPNG(here('data/slide.png'))
 
 ui <- fluidPage(
-
+    
     # Application title
-    titlePanel("Spatial Transcriptomics Plotting"),
-
+    titlePanel("EVD68 Spinal Cord Spatial Transcripts"),
+    
+    tags$p("This spatial transcriptomic data set was created using the GeoMx platform from NanoString, comparing the 
+    transcripts of ~1800 immunology- and oncology-related gene transcripts in the different highlighted regions of interest. 
+    This specimen is human spinal cord from a five-year-old boy who died due to acute flaccid myelitis caused by enterovirus D68 infection. 
+    The colored markers on this tissue help to identify the tissue structure and highlight inflamed areas of tissue:"),
+    
+    tags$ul(
+        tags$li("Green = GFAP"),
+        tags$li("Yellow = CD68"),
+        tags$li("Red = CD3E"),
+        tags$li("Blue = DNA")
+    ),
+    
+    tags$p("This specimen is detailed in two publications:"),
+    tags$ul(
+        tags$li("Kreuter JD, et al. ", a(href='https://doi.org/10.5858/2010-0174-CR.1',"A fatal central nervous system enterovirus 68 infection"),". Arch Pathol Lab Med. 2011 Jun;135(6):793-6."),
+        tags$li("Vogt MR, et al. Submitted")
+    ),
+    
     sidebarLayout(
         sidebarPanel(
             autocomplete_input("gene", 
-                               h2("Select a Gene of Interest"), 
+                               h3("Select a Gene of Interest"), 
                                unique(exp_data$gene),
-                               value = "A2M",
+                               value = "CTSL",
                                placeholder = "Start Typing to Find a Gene",
                                max_options = 100),
+            hr(),
             
-
+            tags$p("Regions of interest:"),
+            tags$ul(
+                tags$li("Inflamed: 001, 005-015"),
+                tags$li("Control: 016-021, 024")
+            ),
+            
+            tags$p("The following perivascular regions of interest were not included in the analysis for the manuscript xxxxxxxx:"),
+            tags$ul(
+                tags$li("Inflamed: 002-004"),
+                tags$li("Control: 022-023")
+            ),
+            hr(),
+            downloadButton("norm_data_download", label = "Download Normalized Data Set"),
+            downloadButton("full_data_download", label = "Download Full Data Set")
+            
+            
         ),
-
+        
         mainPanel(
-           plotOutput("spatial_plot", height="auto")
+            plotOutput("spatial_plot", height="auto")
         )
     ),
     hr(),
@@ -46,7 +83,9 @@ ui <- fluidPage(
         column(12,
                reactableOutput("data_summary")
         )
-    )
+    ),
+    hr(),
+    downloadButton("filt_data_download", label = "Download Filtered Data Set")
     
 )
 
@@ -55,53 +94,73 @@ server <- function(input, output, session) {
     selected_gene_data <- reactive({
         selected_gene = input$gene
         
-        print(session$clientData$output_spatial_plot_width)
-        
         exp_data %>%
             filter(gene == input$gene) %>%
             mutate(exp_alpha = ifelse(is.na(exp), 0, 1))
     })
-
+    
     output$spatial_plot <- renderPlot({
-        ggplot(selected_gene_data(), aes(x = x, y = y, color = exp, size = exp, alpha = exp_alpha)) +
+        slide_overlay = ggplot(selected_gene_data(), aes(x = x, y = y, color = exp, size = exp, alpha = exp_alpha)) +
             background_image(img) +
             geom_point() +
             scale_color_viridis_c() +
-            theme(axis.title.x=element_blank(),
-                  axis.text.x=element_blank(),
-                  axis.ticks.x=element_blank(),
-                  axis.title.y=element_blank(),
-                  axis.text.y=element_blank(),
-                  axis.ticks.y=element_blank(),
-                  text = element_text(size=20),
-                  aspect.ratio=dim(img)[1]/dim(img)[2]) +
+            coord_fixed(ratio = dim(img)[1]/dim(img)[2]) +
+            theme_void() +
+            theme(text = element_text(size=20)) +
             labs(color = "Gene\nExpression\nLevel", size = "Gene\nExpression\nLevel") + 
             scale_alpha(guide = 'none')
-    }, width = function() {
-        if (session$clientData$output_spatial_plot_width > 1000) {
-            1000;
-        } else {
-            session$clientData$output_spatial_plot_width
-        }
-    }, height = function() {
-        if (session$clientData$output_spatial_plot_width > 1000) {
-            800;
-        } else {
-            session$clientData$output_spatial_plot_width * 0.8
-        }
+        
+        
+        data_box = ggplot(selected_gene_data() %>% filter(!is.na(type)), aes(x=type, y=exp)) +
+            geom_boxplot() +
+            labs(x="",y="Normalized Gene Expression") +
+            theme(text = element_text(size=20)) +
+            BerginskiRMisc::theme_berginski()
+        
+        layout <- "
+        AA#B
+        AA#B
+        "
+        
+        slide_overlay + plot_spacer() + data_box +
+            plot_layout(widths = c(4,0.25,1))
+        # plot_layout(design = layout)
+    }, 
+    width = function() {
+        session$clientData$output_spatial_plot_width
+    },
+    height = function() {
+        session$clientData$output_spatial_plot_width * 0.6
     })
+    
+    output$norm_data_download <- downloadHandler(
+        filename = "vogt_deseq2_norm_counts.csv", 
+        content = function(file) {
+            write_csv(read_csv(here('data/vogt_deseq2_norm_counts.csv')), file)
+        } 
+    )
+
+    output$full_data_download <- downloadHandler(
+        filename = "CTA Initial Dataset.xlsx",
+        content = function(file) {
+            file.copy(here('data/all_data/CTA Initial Dataset.xlsx'), file)
+        }
+    )
+    
+    output$filt_data_download <- downloadHandler(
+        filename = paste0("vogt_deseq2_norm_counts_",input$gene,".csv"), 
+        content = function(file) {
+            write_csv(selected_gene_data, file)
+        } 
+    )
     
     output$data_summary <- renderReactable({
         reactable(selected_gene_data() %>%
                       arrange(desc(exp)) %>%
-                      select(location,gene,exp) %>%
+                      select(location,gene,exp,type) %>%
                       filter(location != "pos_099", location != "pos_098"), 
                   filterable = TRUE, pagination = F)
     })
-    
-    # output$sized_plot <- renderUI({
-    #     plotOutput("spatial_plot", height = "auto")
-    # })
 }
 
 # Run the application 
